ab2b6149d6de0a2c1fe5dd9fea06820d4cd9e6af
SaadiaAjbbar <ajbbarsaadia6@gmail.com>
2025-12-23 15:34:24 +0100

    fixer le probleme de session pour profile

--- config/bd.php
+++ config/bd.php
@@ -10,4 +10,3 @@ $connexion = mysqli_connect($server, $user, $password, $dbname);
 if (!$connexion) {
     die("error:".mysqli_connect_error());
 }
-echo "bien connecte";
--- controllers/profile.controller.php
+++ controllers/profile.controller.php
@@ -2,17 +2,17 @@
 $page = "/profile";
 require_once __DIR__ . '/../config/bd.php';
 
-if (!isset($_SESSION['user_id'])) {
+if (!isset($_SESSION['user'])) {
     header("Location: /login");
     exit;
 }
 
-$user_id = $_SESSION['user_id'];
+$user_id = $_SESSION['user']['id'];
 
 $requete = "SELECT name, email, created_at FROM users WHERE id = $user_id";
 $result = mysqli_query($connexion, $requete);
 $user = mysqli_fetch_assoc($result);
-//evite warning si user name null
+
 if (!$user) {
     session_destroy();
     header("Location: /login");
